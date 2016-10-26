61300def242725d1ed96a978be6c4db541593fe8
Taylor Shuff <tkylesh@gmail.com>
2016-10-26 14:11:47 -0500

    set up initial call to open weather api using zip code, still working to get a response.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,35 @@
 "use strict";
+let apiKey = {};
+let getWeather = (zip) => {
+	return new Promise((resolve, reject)=> {
+		$.ajax({
+			method:'GET',
+			url:'apiKey.json'
+		}).then((response) => {
+			apiKey = response;
+			let appID = 'APPID='+apiKey.Key;
+
+			$.ajax({
+				method:'GET',
+				url:`api.openweathermap.org/data/2.5/weather?zip=${zip},us&${appID}`
+			}).then((response) => {
+				console.log('weatherData: ',response);
+				// resolve(response);
+			}, (errorResponseWeather) => {
+				console.log('errorResponseWeather',errorResponseWeather);
+				// reject(errorResponseWeather);
+			});
+		}, (errorResponseKey)=> {
+			console.log('errorResponseKey',errorResponseKey);
+			// reject(errorResponseKey);
+		});
+	});
+};
 
 $(document).ready(() => {
 	console.log('jquery is ready');
+	getWeather(38401).then((weatherData) => {
+		console.log('data from weather api: ',weatherData);
+	});
 
 });
